8b9bee4dda6ed641e492ccfcd020e7081ac87391
picashuo <popoqoqo2003@gmail.com>
2020-06-30 16:41:25 +0800

    【更新】Densenet_YOLOv3_train.py
    新增SENet Layer Freeze

--- keras-yolo3/Densenet_YOLOv3_train.py
+++ keras-yolo3/Densenet_YOLOv3_train.py
@@ -4,12 +4,13 @@ Retrain the YOLO_densenet model for your own dataset.
 
 import numpy as np
 import keras.backend as K
+from keras.callbacks import LambdaCallback
 from keras.layers import Input, Lambda
 from keras.models import Model
 from keras.optimizers import Adam
 from keras.callbacks import TensorBoard, ModelCheckpoint, ReduceLROnPlateau, EarlyStopping
 
-from yolo3.model import preprocess_true_boxes, yolo_body, tiny_yolo_body, yolo_loss
+from yolo3.model import preprocess_true_boxes, yolo_body, yolo_loss
 from yolo3.utils import get_random_data
 from yolo3.model_densenet import densenet_body
 from yolo3.model_se_densenet import se_densenet_body
@@ -37,6 +38,14 @@ def _main():
     model = create_model(input_shape, anchors, num_classes,freeze_body=2,
     load_pretrained=load_pretrained, weights_path=log_dir+'ep'+str(load_file)+'.h5') # make sure you know what you freeze
     logging = TensorBoard(log_dir=log_dir)
+
+    checkpoint = ModelCheckpoint(log_dir +'s'+str(load_file)+'_'+'ep{epoch:03d}-loss{loss:.3f}-val_loss{val_loss:.3f}.h5',
+        monitor='val_loss', save_weights_only=True, save_best_only=True, period=3)
+
+    # define your custom callback for prediction
+    batch_print_callback = LambdaCallback(
+        on_epoch_end=lambda epoch,logs: print(epoch))
+
     checkpoint = ModelCheckpoint(log_dir +'s'+str(load_file)+'_'+'ep{epoch:03d}-loss{loss:.3f}-val_loss{val_loss:.3f}.h5',
         monitor='val_loss', save_weights_only=True, save_best_only=True, period=3)
 
@@ -53,12 +62,11 @@ def _main():
     np.random.shuffle(val_lines)
     np.random.seed(None)
 
-    #num_val = int(len(lines)*val_split)
+    # num_val = int(len(val_lines))
+    # num_train = len(lines)
 
-    num_val = int(len(val_lines))
-    num_train = len(lines)
-
-    #num_train = len(lines) - num_val
+    num_val = int(len(lines)*val_split)
+    num_train = len(lines) - num_val
     #print(len(lines))
     # print(lines[:num_train])
     # print(lines[num_train:])
@@ -67,25 +75,26 @@ def _main():
             model.layers[i].trainable = True
         model.compile(optimizer=Adam(lr=1e-4), loss={'yolo_loss': lambda y_true, y_pred: y_pred}) # recompile to apply the change
         # print('Unfreeze all of the layers.')
-        #model.save(log_dir + 'trained_weights_final2020031ALLmodel.h5')
+        model.save(log_dir + 'network.h5')
 
         #batch_size = 8 # note that more GPU memory is required after unfreezing the body
         print('Train on {} samples, val on {} samples, with batch size {}.'.format(num_train, num_val, batch_size))
-        model.fit_generator(data_generator_wrapper(lines[:num_train], batch_size, input_shape, anchors, num_classes),
-            steps_per_epoch=max(1, num_train//batch_size),
-            validation_data=data_generator_wrapper(val_lines[:len(val_lines)], batch_size, input_shape, anchors, num_classes),
-            validation_steps=max(1, num_val//batch_size),
-            epochs=epoch,
-            initial_epoch=0,
-            callbacks=[logging, checkpoint])
-
+        #For Google用
         # model.fit_generator(data_generator_wrapper(lines[:num_train], batch_size, input_shape, anchors, num_classes),
         #     steps_per_epoch=max(1, num_train//batch_size),
-        #     validation_data=data_generator_wrapper(lines[num_train:], batch_size, input_shape, anchors, num_classes),
+        #     validation_data=data_generator_wrapper(val_lines[:len(val_lines)], batch_size, input_shape, anchors, num_classes),
         #     validation_steps=max(1, num_val//batch_size),
         #     epochs=epoch,
         #     initial_epoch=0,
         #     callbacks=[logging, checkpoint])
+        #For 測試用
+        model.fit_generator(data_generator_wrapper(lines[:num_train], batch_size, input_shape, anchors, num_classes),
+            steps_per_epoch=max(1, num_train//batch_size),
+            validation_data=data_generator_wrapper(lines[num_train:], batch_size, input_shape, anchors, num_classes),
+            validation_steps=max(1, num_val//batch_size),
+            epochs=epoch,
+            initial_epoch=0,
+            callbacks=[logging, checkpoint,batch_print_callback])
             #callbacks=[logging, checkpoint, reduce_lr, early_stopping])
         model.save_weights(log_dir + 'ep'+str(int(load_file)+int(epoch))+'.h5')
 
@@ -117,14 +126,17 @@ def create_model(input_shape, anchors, num_classes, load_pretrained=True, freeze
 
     y_true = [Input(shape=(h//{0:32, 1:16, 2:8}[l], w//{0:32, 1:16, 2:8}[l], \
         num_anchors//3, num_classes+5)) for l in range(3)]
-
+    # print(y_true)
+    # return
     if modeltype == "YOLOV3":
         model_body = yolo_body(image_input, num_anchors//3, num_classes)
     if modeltype == "YOLOV3Densenet":
         model_body = densenet_body(image_input, num_anchors//3, num_classes)
     if modeltype == "YOLOV3SE-Densenet":
         model_body = se_densenet_body(image_input, num_anchors//3, num_classes)
-
+    if modeltype == "SE-YOLOV3":
+        model_body = yolo_body(image_input, num_anchors//3, num_classes,"SE-YOLOV3")
+    
     print('Create YOLOv3 model with {} anchors and {} classes.'.format(num_anchors, num_classes))
        
     if load_pretrained:
@@ -136,14 +148,24 @@ def create_model(input_shape, anchors, num_classes, load_pretrained=True, freeze
                 num = len(model_body.layers)-7
                 for i in range(num): model_body.layers[i].trainable = False
                 print('Freeze the first {} layers of total {} layers.'.format(num, len(model_body.layers)))
+        if modeltype == "SE-YOLOV3":
+            if freeze_body:
+                # Do not freeze 3 output layers.
+                num = len(model_body.layers)-7
+                for i in range(num): model_body.layers[i].trainable = False
+                print('Freeze the first {} layers of total {} layers.'.format(num, len(model_body.layers)))
         if modeltype == "YOLOV3Densenet":
             if freeze_body in [1, 2]:
                 # Freeze darknet53 body or freeze all but 3 output layers.
                 num = 424
                 for i in range(num): model_body.layers[i].trainable = False
                 print('Freeze the first {} layers of total {} layers.'.format(num, len(model_body.layers)))
-
-
+        if modeltype == "YOLOV3SE-Densenet":
+            if freeze_body in [1, 2]:
+                # Freeze darknet53 body or freeze all but 3 output layers.
+                num = 424
+                for i in range(num): model_body.layers[i].trainable = False
+                print('Freeze the first {} layers of total {} layers.'.format(num, len(model_body.layers)))
     model_loss = Lambda(yolo_loss, output_shape=(1,), name='yolo_loss',
         arguments={'anchors': anchors, 'num_classes': num_classes, 'ignore_thresh': 0.5})(
         [*model_body.output, *y_true])
